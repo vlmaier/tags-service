60ba4770e2249fe6dad762eaf7114cc828005f59
Vladas Maier <v.maier@dracoon.com>
2022-12-28 21:09:56 +0100

    Initial commit

---
+++ src/main/kotlin/com/vmaier/marvel/snap/tags/TagsServiceApp.kt
@@ -0,0 +1,11 @@
+package com.vmaier.marvel.snap.tags
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TagsServiceApp
+
+fun main(args: Array<String>) {
+    runApplication<TagsServiceApp>(*args)
+}
---
+++ src/test/kotlin/com/vmaier/marvel/snap/tags/TagsServiceAppTests.kt
@@ -0,0 +1,13 @@
+package com.vmaier.marvel.snap.tags
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TagsServiceAppTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
