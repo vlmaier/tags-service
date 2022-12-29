21f99e744f070ce883040141041bd8290cfb7246
Vladas Maier <v.maier@dracoon.com>
2022-12-29 23:55:04 +0100

    Add first tagging logic

--- src/main/kotlin/com/vmaier/marvel/snap/tags/service/TagsService.kt
+++ src/main/kotlin/com/vmaier/marvel/snap/tags/service/TagsService.kt
@@ -1,23 +1,75 @@
 package com.vmaier.marvel.snap.tags.service
 
+import com.vmaier.marvel.snap.tags.db.dao.CardTag
+import com.vmaier.marvel.snap.tags.db.repo.CardTagRepository
+import com.vmaier.marvel.snap.tags.db.repo.TagRepository
+import com.vmaier.marvel.snap.tags.model.CardResponse
 import jakarta.transaction.Transactional
-import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.util.*
+import java.util.stream.Stream
 
 @Service
 @Transactional
 class TagsService(
-    private val restTemplateService: RestTemplateService
+    private val restTemplateService: RestTemplateService,
+    private val tagRepository: TagRepository,
+    private val cardTagRepository: CardTagRepository
 ) {
 
-    private val logger = KotlinLogging.logger {}
+    fun createTags() {
+        val cards = getCards()
+        val cardTags = mutableListOf<CardTag>()
+        for (card in cards.toList()) {
+            val tags = findTags(card)
+            for (tag in tags) {
+                cardTags.add(CardTag(tag.key, card.name))
+            }
+        }
+        cardTagRepository.saveAll(cardTags)
+    }
 
-    fun getCards() {
+    fun getCards(): List<CardResponse> {
         val response = restTemplateService.getCards()
         if (response.statusCode == HttpStatus.OK) {
             val cards = response.body
+            if (!cards.isNullOrEmpty()) {
+                return cards.toList()
+            }
+        }
+        return listOf()
+    }
 
+    fun findTags(card: CardResponse): Map<Int, String> {
+        val tags = mutableMapOf<Int, String>()
+        findTag(card, tags, "On Reveal", "On Reveal:")
+        findTag(card, tags, "Ongoing", "Ongoing:")
+        findTag(card, tags, "Move", "Move")
+        findTag(card, tags, "Add Card", "Add", "Card")
+        findTag(card, tags, "Destroy", "Destroy")
+        // findTag(card, tags, "Stack Power", "")
+        findTag(card, tags, "Discard", "Discard")
+        findTag(card, tags, "Draw Card", "Draw", "Card")
+        // findTag(card, tags, "Manipulate Power", "")
+        // findTag(card, tags, "Manipulate Cost", "")
+        // findTag(card, tags, "Conditional", "")
+        // findTag(card, tags, "No Ability", "")
+        return tags
+    }
+
+    fun findTag(card: CardResponse, tags: MutableMap<Int, String>, tagName: String, vararg keywords: String) {
+        val allTags = tagRepository.findAll()
+        if (card.ability.isNullOrEmpty()) {
+            return
+        }
+        val abilityWordList = card.ability.split(" ").toList()
+        if (keywords.asList().stream().allMatch(abilityWordList::contains)) {
+            val tagId = allTags.find { t -> t.name == tagName }?.id ?: 0
+            if (tagId == 0) {
+                return
+            }
+            tags[tagId] = card.name
         }
     }
 }
--- src/main/resources/db/migration/V1_0_0__Initial.sql
+++ src/main/resources/db/migration/V1_0_0__Initial.sql
@@ -13,3 +13,17 @@ CREATE TABLE card_tags
         FOREIGN KEY (tag_id)
             REFERENCES tags (id)
 );
+
+INSERT INTO tags
+VALUES (1, 'On Reveal'),
+       (2, 'Ongoing'),
+       (3, 'Move'),
+       (4, 'Add Card'),
+       (5, 'Destroy'),
+       (6, 'Stack Power'),
+       (7, 'Discard'),
+       (8, 'Draw Card'),
+       (9, 'Manipulate Power'),
+       (10, 'Manipulate Cost'),
+       (11, 'Conditional'),
+       (12, 'No Ability');
